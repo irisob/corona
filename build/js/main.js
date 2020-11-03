7d7493399ce4daa1da2e7fbf0a267bf49a380db6
irisob <irinizobel@gmail.com>
2020-11-03 21:27:22 +0100

    add reading json

--- build/js/main.js
+++ build/js/main.js
@@ -1,53 +1,21 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+async function getJSON() {
+  let response = await fetch('https://irisob.github.io/corona/build/data/data.json');
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-// load in JSON data from file
-var data;
-var oReq = new XMLHttpRequest();
-oReq.onload = reqListener;
-oReq.open("get", "data/data.json", true);
-oReq.send();
-
-function reqListener(e) {
-  data = JSON.parse(this.responseText);
+  if (response.ok) {
+    let json = await response.json();
+    console.log(json);
+    return json;
+  } else {
+    console.log(response.ok);
+  }
 }
 
-console.log(data);
+var data = getJSON();
 
-var App = /*#__PURE__*/function (_React$Component) {
-  _inherits(App, _React$Component);
-
-  var _super = _createSuper(App);
-
-  function App() {
-    _classCallCheck(this, App);
-
-    return _super.apply(this, arguments);
-  }
-
-  _createClass(App, [{
-    key: "render",
+class App extends React.Component {
+  render() {
     // var element = (
     //   <table className="table">
     //       <thead>
@@ -67,15 +35,12 @@ var App = /*#__PURE__*/function (_React$Component) {
     //       </tbody>
     //   </table>
     // );
-    value: function render() {
-      return /*#__PURE__*/React.createElement("div", {
-        className: "container"
-      }, "Meow");
-    }
-  }]);
+    return /*#__PURE__*/React.createElement("div", {
+      className: "container"
+    }, "Meow");
+  }
 
-  return App;
-}(React.Component);
+}
 
 ;
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.querySelector("#main"));
--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,44 @@
-// load in JSON data from file
-var data;
 
-var oReq = new XMLHttpRequest();
-oReq.onload = reqListener;
-oReq.open("get", "data/data.json", true);
-oReq.send();
 
-function reqListener(e) {
-    data = JSON.parse(this.responseText);
+async function getJSON() {
+  let response = await fetch('https://irisob.github.io/corona/build/data/data.json');
+
+  if (response.ok) {
+    let json = await response.json();
+    console.log(json);
+    return json;
+  } else {
+    console.log(response.ok);
+  }
 }
-console.log(data);
+
+
+var data = getJSON();
 
 class App extends React.Component {
 
-
-  // var element = (
-  //   <table className="table">
-  //       <thead>
-  //           <tr>
-  //               <th>Страна</th>
-  //               <th>Число случаев за 7 дней на 100 тыс.</th>
-  //               <th>Неделю назад</th>
-  //               <th>2 Недели назад</th>
-  //               <th>Эмулированное по смертям (макс)</th>
-  //               <th>Эмулированное по смертям (диапазон)</th>
-  //               <th>По отношению к прошлой неделе</th>
-  //               <th>Динамика за 7 дней</th>
-  //           </tr>
-  //       </thead>
-  //       <tbody>
-  //
-  //       </tbody>
-  //   </table>
-  // );
-
   render() {
+
+    // var element = (
+    //   <table className="table">
+    //       <thead>
+    //           <tr>
+    //               <th>Страна</th>
+    //               <th>Число случаев за 7 дней на 100 тыс.</th>
+    //               <th>Неделю назад</th>
+    //               <th>2 Недели назад</th>
+    //               <th>Эмулированное по смертям (макс)</th>
+    //               <th>Эмулированное по смертям (диапазон)</th>
+    //               <th>По отношению к прошлой неделе</th>
+    //               <th>Динамика за 7 дней</th>
+    //           </tr>
+    //       </thead>
+    //       <tbody>
+    //
+    //       </tbody>
+    //   </table>
+    // );
+
     return (
         <div  className="container">Meow</div>
     );
